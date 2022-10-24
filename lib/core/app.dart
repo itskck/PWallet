9e3e632a8d34868b0a057ef9c9469e2f5ad6bddd
Juliusz Piskor <juliuszpiskor@gmail.com>
2022-10-24 19:14:04 +0200

    fixed some bugs

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pwallet/core/global_providers.dart';
 import 'package:pwallet/core/router.dart';
 import 'package:pwallet/ui/screens/login_screen.dart';
+import 'package:pwallet/utils/no_transitions_builder.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -13,6 +15,13 @@ class App extends StatelessWidget {
       child: MaterialApp.router(
         theme: ThemeData(
           colorSchemeSeed: Color.fromARGB(255, 83, 57, 126),
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              // No animations for every OS if the app running on the web
+              for (final platform in TargetPlatform.values)
+                platform: const NoTransitionsBuilder(),
+            },
+          ),
         ),
         debugShowCheckedModeBanner: false,
         title: 'Password wallet',
--- lib/ui/screens/password_add_screen.dart
+++ lib/ui/screens/password_add_screen.dart
@@ -14,91 +14,97 @@ class PasswordAddScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 50),
-                child: Text(
-                  'Add new password',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline2,
+    return WillPopScope(
+      onWillPop: () async {
+        GoRouter.of(context).pop();
+        return true;
+      },
+      child: Scaffold(
+        body: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 50),
+                  child: Text(
+                    'Add new password',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline2,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: TextField(
-                  controller: addressController,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: TextField(
+                    controller: addressController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Web address',
+                      hintText: 'Enter web page address',
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: descriptionController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'Web address',
-                    hintText: 'Enter web page address',
+                    labelText: 'Description',
+                    hintText: 'Enter description',
                   ),
                 ),
-              ),
-              TextField(
-                controller: descriptionController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Description',
-                  hintText: 'Enter description',
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: TextField(
+                    controller: loginController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Login',
+                      hintText: 'Enter your login',
+                    ),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: TextField(
-                  controller: loginController,
+                TextField(
+                  controller: passwordController,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'Login',
-                    hintText: 'Enter your login',
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
                   ),
                 ),
-              ),
-              TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      GoRouter.of(context).pop();
-                    },
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (isFormReady) {
-                        BlocProvider.of<UserCubit>(context).addPassword(
-                          password: passwordController.text,
-                          address: addressController.text,
-                          description: descriptionController.text,
-                          login: loginController.text,
-                        );
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
                         GoRouter.of(context).pop();
-                        showGoodToast('Password added');
-                      } else {
-                        showBadToast('All fields should be filled');
-                      }
-                    },
-                    child: const Text('Add'),
-                  ),
-                ],
-              )
-            ],
+                      },
+                      child: const Text('Cancel'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (isFormReady) {
+                          BlocProvider.of<UserCubit>(context).addPassword(
+                            password: passwordController.text,
+                            address: addressController.text,
+                            description: descriptionController.text,
+                            login: loginController.text,
+                          );
+                          GoRouter.of(context).pop();
+                          showGoodToast('Password added');
+                        } else {
+                          showBadToast('All fields should be filled');
+                        }
+                      },
+                      child: const Text('Add'),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
